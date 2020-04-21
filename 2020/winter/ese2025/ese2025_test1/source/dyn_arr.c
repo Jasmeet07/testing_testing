ae335abbbd5ee919db6b0006412ae7ae5de31f2a
Jasmeet <57648368+Jasmeet07@users.noreply.github.com>
2020-04-20 20:05:18 -0400

    Update dyn_arr.c

--- 2020/winter/ese2025/ese2025_test1/source/dyn_arr.c
+++ 2020/winter/ese2025/ese2025_test1/source/dyn_arr.c
@@ -98,8 +98,7 @@ stuDA_t* formDynamicArray(stuLL_t *pHEAD_LL, size_t student_count)
 stuDA_t* sortDynamicArray(stuDA_t *pHEAD, const sort_t sort_type,
 		const size_t array_size)
 {
-	// PROBLEM #5: complete the code for this function
-	//	       NOTE: PLEASE COMMIT CHANGES ONCE YOUR DONE THIS FUNCTION
+
 
 	switch (sort_type)
 	{
@@ -108,8 +107,10 @@ stuDA_t* sortDynamicArray(stuDA_t *pHEAD, const sort_t sort_type,
 		break;
 	case gNamesort:
 		qsort(pHEAD, array_size, sizeof(stuRec_t), comparo_gName);
+		break;
 	case gradesort:
 		qsort(pHEAD, array_size, sizeof(stuRec_t), comparo_grade);
+		break;
 	}
 	return pHEAD;
 }
